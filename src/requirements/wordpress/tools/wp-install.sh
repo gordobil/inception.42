06c1c5f866d1e6c2028baca77d00f08e6405eb69
gordobil <noahgordobil@gmail.com>
2026-01-30 19:32:19 +0100

    urduliz

--- src/requirements/wordpress/tools/wp-install.sh
+++ src/requirements/wordpress/tools/wp-install.sh
@@ -12,7 +12,7 @@ done
 if [ ! -f wp-config.php ]; then
 	./wp-cli.phar core download --allow-root
 	./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-	./wp-cli.phar core install --url=localhost --title=inception --admin_user=ngordobi --admin_password=gordobil --admin_email=ngordobi@inception.com --allow-root
+	./wp-cli.phar core install --url=ngordobi.42.fr --title=inception --admin_user=ngordobi --admin_password=gordobil --admin_email=ngordobi@inception.com --allow-root
 	./wp-cli.phar user create guest guest@inception.com --role=subscriber --user_pass=12345 --allow-root
 fi
 
